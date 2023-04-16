9a6c4658680ab4c8317606c831e9146ebf3a170d
longlom <long.lom@yandex.ru>
2023-04-16 18:17:25 +0300

    STM: managed to set pwm on d9

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-//! Basic example that produces a 1Hz square-wave on Pin PE1
-
 #![deny(warnings)]
 #![no_main]
 #![no_std]
@@ -17,76 +15,79 @@ fn panic(_info: &PanicInfo) -> ! {
     }
 }
 
-#[entry]
-fn main() -> ! {
-    let _cp = cortex_m::Peripherals::take().unwrap();
+fn _pwm_d9() -> ! {
+    let cp = cortex_m::Peripherals::take().unwrap();
     let dp = pac::Peripherals::take().unwrap();
 
     // Constrain and Freeze power
     // hprintln!("Setup PWR...                  ").unwrap();
     let pwr = dp.PWR.constrain();
     let pwrcfg = pwr.smps().freeze();
+
     // Constrain and Freeze clock
-    // hprintln!("Setup RCC...                  ").unwrap();
     let rcc = dp.RCC.constrain();
     let ccdr = rcc.sys_ck(100.MHz()).freeze(pwrcfg, &dp.SYSCFG);
 
-    // hprintln!("").unwrap();
-    // hprintln!("stm32h7xx-hal example - Blinky").unwrap();
-    // hprintln!("").unwrap();
-
-    // let gpioc = dp.GPIOC.split(ccdr.peripheral.GPIOC);
-    // let gpiob = dp.GPIOB.split(ccdr.peripheral.GPIOB);
-    // let gpioe = dp.GPIOE.split(ccdr.peripheral.GPIOE);
-
+    // Get the delay provider.
+    let mut delay = cp.SYST.delay(ccdr.clocks);
     // Configuring pwm
     let tim4 = dp.TIM4;
     let gpiob = dp.GPIOB.split(ccdr.peripheral.GPIOB);
 
-    // Select PWM output pins
-    // let pins = (
-    //     // gpioa.pa8.into_alternate(),
-    //     // gpioa.pa9.into_alternate(),
-    //     // gpiob.pb7.into_alternate(),
-    // );
-        
     let pb7 = gpiob.pb7.into_alternate();
 
     // Configure PWM at 10kHz
-    let mut pwm =  tim4
-            .pwm(pb7, 10.kHz(), ccdr.peripheral.TIM4, &ccdr.clocks);
+    let mut pwm = tim4.pwm(pb7, 10.kHz(), ccdr.peripheral.TIM4, &ccdr.clocks);
 
-    // Output PWM on PA8
+    // Output PWM on PB7
     let max_duty = pwm.get_max_duty();
+    let min_duty = max_duty / 100;
     pwm.set_duty(max_duty);
-
-    // let mut p = dp
-    //     .TIM4
-    //     .pwm(pd14, 10.kHz(), ccdr.peripheral.TIM4, &ccdr.clocks);
-
+    pwm.enable();
     hprintln!("PWM SET").unwrap();
+    loop {
+        delay.delay_ms(1000_u16);
+        pwm.set_duty(max_duty);
+        delay.delay_ms(1000_u16);
+        pwm.set_duty(min_duty);
+    }
+}
 
-    // let (mut pwm, ..) =
-    // dp.TIM1
-    //     .pwm(pins, 10.kHz(), ccdr.peripheral.TIM1, &ccdr.clocks);
-    // let stmod = gpioe.pe9.into_pull_up_input();
-    // Configure PE1 as output.
-    // let mut led = gpioc.pc3.into_push_pull_output();
-    // let d1_port = gpiob.pb15.into_pull_up_input();
+#[entry]
+fn main() -> ! {
+    let cp = cortex_m::Peripherals::take().unwrap();
+    let dp = pac::Peripherals::take().unwrap();
+
+    // Constrain and Freeze power
+    // hprintln!("Setup PWR...                  ").unwrap();
+    let pwr = dp.PWR.constrain();
+    let pwrcfg = pwr.smps().freeze();
+
+    // Constrain and Freeze clock
+    let rcc = dp.RCC.constrain();
+    let ccdr = rcc.sys_ck(100.MHz()).freeze(pwrcfg, &dp.SYSCFG);
 
     // Get the delay provider.
-    // let mut delay = cp.SYST.delay(ccdr.clocks);
+    let mut delay = cp.SYST.delay(ccdr.clocks);
+
+    let gpioc = dp.GPIOC.split(ccdr.peripheral.GPIOC);
+    // let gpiob = dp.GPIOB.split(ccdr.peripheral.GPIOB);
+    // let gpioe = dp.GPIOE.split(ccdr.peripheral.GPIOE);
+
+    // let stmod = gpioe.pe9.into_pull_up_input();
+    // Configure PE1 as output.
+    let mut led = gpioc.pc3.into_push_pull_output();
+    // let d1_port = gpiob.pb15.into_pull_up_input();
 
     loop {
-        cortex_m::asm::nop()
         // let result = d1_port.is_high();
         // hprintln!("Result - {}", result).unwrap();
         // let result = stmod.is_high();
         // hprintln!("Result - {}", result).unwrap();
-        // led.set_high();
-        // delay.delay_ms(1000_u16);
+        led.set_high();
+        delay.delay_ms(1000_u16);
 
-        // led.set_low();
-        // delay.delay_ms(1000_u16);
+        led.set_low();
+        delay.delay_ms(1000_u16);
     }
 }
