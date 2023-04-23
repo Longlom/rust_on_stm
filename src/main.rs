81b4b4c0788a559ddf0eb52d2f2705986ae4182c
longlom <long.lom@yandex.ru>
2023-04-23 13:43:50 +0300

    STM: add hello-world

--- src/main.rs
+++ src/main.rs
@@ -52,7 +52,7 @@ fn _pwm_d9() -> ! {
     }
 }
 
-fn pwm_stmod() -> ! {
+fn _pwm_stmod() -> ! {
     let cp = cortex_m::Peripherals::take().unwrap();
     let dp = pac::Peripherals::take().unwrap();
 
@@ -76,20 +76,30 @@ fn pwm_stmod() -> ! {
     let mut pwm = tim4.pwm(pd14, 50.Hz(), ccdr.peripheral.TIM4, &ccdr.clocks);
 
     // Output PWM on pd14 (on fanout board)
-    let max_duty = pwm.get_max_duty() / 2;
-    let min_duty = max_duty / 100;
-    pwm.set_duty(max_duty);
+    // let max_duty = pwm.get_max_duty() / 2;
+    let middle = 4915;
+    let max = 6553;
+    let min = 1;
+    // pwm.set_duty(max_duty);
     pwm.enable();
     hprintln!("PWM for fanout SET").unwrap();
     loop {
-        delay.delay_ms(1000_u16);
-        pwm.set_duty(max_duty);
-        delay.delay_ms(1000_u16);
-        pwm.set_duty(min_duty);
+        pwm.set_duty(middle);
+
+        hprintln!("Set middle").unwrap();
+        // Wait for a few seconds
+        delay.delay_ms(3000_u16);
+
+        pwm.set_duty(max);
+        hprintln!("Set max").unwrap();
+        delay.delay_ms(3000_u16);
+
+        pwm.set_duty(min);
+        hprintln!("Set min").unwrap();
+        delay.delay_ms(3000_u16);
+
     }
 }
-
-
 
 fn _blink_ld2() -> ! {
     let cp = cortex_m::Peripherals::take().unwrap();
@@ -118,11 +128,36 @@ fn _blink_ld2() -> ! {
     }
 }
 
+
+
+fn hello_world() -> ! {
+    let cp = cortex_m::Peripherals::take().unwrap();
+    let dp = pac::Peripherals::take().unwrap();
+
+    // Constrain and Freeze power
+    let pwr = dp.PWR.constrain();
+    let pwrcfg = pwr.smps().freeze();
+
+    // Constrain and Freeze clock
+    let rcc = dp.RCC.constrain();
+    let ccdr = rcc.sys_ck(100.MHz()).freeze(pwrcfg, &dp.SYSCFG);
+
+    // Get the delay provider.
+    let mut _delay = cp.SYST.delay(ccdr.clocks);
+
+hprintln!("HELLO WORLD!").unwrap();
+
+    loop {
+
+    }
+}
+
 #[entry]
 fn main() -> ! {
-    pwm_stmod();
-//    blink_ld2();
- //    pwm_d9()
+    hello_world()
+    // pwm_stmod();
+    //    blink_ld2();
+    //    pwm_d9()
 
-//  loop { }
+    //  loop { }
 }
